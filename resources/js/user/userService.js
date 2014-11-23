d8b5328153a639809373b7388531903753714f90
Michael Power <mpower@alumni.calpoly.edu>
2014-11-23 00:53:13 -0800

    Remove partial code to add user

--- resources/js/user/userService.js
+++ resources/js/user/userService.js
@@ -4,14 +4,6 @@
     echadZeman.factory('userService', ['validator', 'scrypt', 'userDao', function(validator, scrypt, userDao) {
         var userService = {
             createUser: function(emailAddress, password, repeatPassword, cb) {
-                var loggingCb = function(err, response) {
-                    console.log(err);
-                    console.log(response);
-                    cb(err, response);
-                }
-                userDao.create({email: emailAddress, password: password}, loggingCb);
-                console.log(validator);
-                console.log(scrypt);
             }
         };
         return userService;
